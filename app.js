128df5074ec23cf252442141e884eca81acfa8a2
Paz777 <111736251+Paz777@users.noreply.github.com>
2023-11-03 08:08:51 +0000

    basic game logic

--- app.js
+++ app.js
@@ -4,24 +4,51 @@ const ROCK = 'ROCK';
 const PAPER = 'PAPER';
 const SCISSORS = 'SCISSORS';
 const DEFAULT_USER_CHOICE = ROCK;
+const RESULT_DRAW = "DRAW";
+const RESULT_PLAYER_WINS = "PLAYER_WINS";
+const RESULT_COMPUTER_WINS = "COMPUTER_WINS";
 let gameIsRunning = false;
 
-const getPlayerChoice = function() {
+const getPlayerChoice = function () {
     const selection = prompt(`${ROCK}, ${PAPER} or ${SCISSORS}`, '').toUpperCase();
-    if (selection !== ROCK && selection !== PAPER && selection !== SCISSORS)
-    {
+    if (selection !== ROCK && selection !== PAPER && selection !== SCISSORS) {
         alert(`Invalid choice, we choose ${DEFAULT_USER_CHOICE} for you`);
         return DEFAULT_USER_CHOICE;
     }
     return selection;
-}
+};
 
-startGameButton.addEventListener('click', function() {
+const getComputerChoice = function () {
+    const randomValue = Math.random();
+    if (randomValue < 0.34) {
+        return ROCK;
+    } else if (randomValue < 0.67) {
+        return PAPER;
+    } else {
+        return SCISSORS;
+    }
+};
+
+const getWinner = function (computerChoice, playerChoice) {
+    if (computerChoice === playerChoice) {
+        return RESULT_DRAW;
+    } else if (computerChoice === ROCK && playerChoice === PAPER ||
+        computerChoice === PAPER && playerChoice === SCISSORS ||
+        computerChoice === SCISSORS && playerChoice === ROCK) {
+        return RESULT_PLAYER_WINS;
+    } else {
+        return RESULT_COMPUTER_WINS;
+    }
+};
+
+startGameButton.addEventListener('click', function () {
     if (gameIsRunning) {
         return;
     }
     gameIsRunning = true;
     console.log('game starting ...');
-    const playerSelection = getPlayerChoice();
-    console.log(playerSelection);
+    const playerChoice = getPlayerChoice();
+    const computerchoice = getComputerChoice();
+    const winner = getWinner(computerchoice, playerChoice);
+    console.log(winner);
 });
